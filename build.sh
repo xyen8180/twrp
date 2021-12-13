1da434a8811a7c2818a0c1135bbf801663e90f54
xyen8180 <xyen8180@gmail.com>
2021-12-14 01:46:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,14 +20,16 @@ echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST
 repo sync
 repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
-git clone --depth=1 $SD_LINK $SD_PATH
-git clone http://github.com/xyen8180/vendor_star vendor/xiaomi/sm8350-common/
-git clone https://github.com/nebrassy/kernel_xiaomi_sm8350  kernel/xiaomi/sm8350/
+#git clone --depth=1 $DT_LINK $DT_PATH
+#git clone --depth=1 $SD_LINK $SD_PATH
+#git clone http://github.com/xyen8180/vendor_star vendor/xiaomi/sm8350-common/
+#git clone https://github.com/nebrassy/kernel_xiaomi_sm8350  kernel/xiaomi/sm8350/
 echo " ===+++ Building Recovery +++==="
 chmod u+x ./device/xiaomi/star/*
 echo -------------------------------
+cd build
 ls
+cd -
 echo -------------------------------
 cd device/xiaomi/star/
 . build/envsetup.sh
