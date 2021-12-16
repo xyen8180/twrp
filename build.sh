486071b544181b89154909f1e6c1b85a889909df
xyen8180 <xyen8180@gmail.com>
2021-12-16 16:47:50 +0530

    adding titles

--- build.sh
+++ build.sh
@@ -17,12 +17,17 @@ apt install openssh-server -y
 mkdir ~/twrp && cd ~/twrp
 
 echo " ===+++ Syncing Recovery Sources +++==="
+echo "Cloning Manifest"
 repo init --depth=1 -u $MANIFEST 2>/dev/null
 repo sync
 repo sync
+echo "Device Tree Manifest"
 git clone --depth=1 $DT_LINK $DT_PATH 2>/dev/null
+echo "Cloning SM8350-common Tree"
 git clone --depth=1 $SD_LINK $SD_PATH 2>/dev/null
+echo "Cloning VEndor Manifest"
 git clone http://github.com/xyen8180/vendor_star vendor/xiaomi/sm8350-common/ 2>/dev/null
+echo "Cloning Kernel Tree"
 git clone https://github.com/nebrassy/kernel_xiaomi_sm8350  kernel/xiaomi/sm8350/ 2>/dev/null
 echo " ===+++ Building Recovery +++==="
 chmod -R u+x *
